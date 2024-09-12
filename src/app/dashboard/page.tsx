f2ff5da415500021c377488b32e672a9eb9d0b6c
nihar-hegde <niharhegde163@gmail.com>
2024-09-12 21:19:48 +0530

    login working for now

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,20 @@
+import { auth } from "@/lib/auth/auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+const DashboardPage = async () => {
+  const session = await auth();
+  if (!session) {
+    redirect("/auth/sign-in");
+  }
+  console.log(session);
+  return (
+    <div>
+      <h1>Welcome to your Dashboard, {session?.user?.name}!</h1>
+      <p>Your email: {session?.user?.email}</p>
+      <img src={session?.user?.image} alt="Profile" width={50} height={50} />
+    </div>
+  );
 };
 
 export default DashboardPage;
